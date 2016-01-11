23bbad63030f656a2b2b6198a904ab5dc9e7bba3
Andrew Ray <ray.andrew@gmail.com>
2016-01-11 12:55:09 -0600

    Cli dont specify master, let it get set by spark-submit. Use HiveContext.

--- src/main/scala/com/databricks/spark/sql/perf/Cli.scala
+++ src/main/scala/com/databricks/spark/sql/perf/Cli.scala
@@ -2,7 +2,8 @@ package com.databricks.spark.sql.perf
 
 import com.databricks.spark.sql.perf.tpcds.Tables
 import com.databricks.spark.sql.perf.tpcds.TPCDS
-import org.apache.spark.SparkContext
+import org.apache.spark.sql.hive.HiveContext
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.SQLContext
 
 /**
@@ -13,22 +14,26 @@ object Cli {
   def usage() = {
     println(
       """spark-sql-perf cli usage
-        |Generate tables: Cli generate <master> <table-path> <format> <scale-factor> <tpc-ds-tools>
-        |Run query set:   Cli run <master> <table-path> <format>
+        |Generate tables: Cli generate <table-path> <format> <scale-factor> <tpc-ds-tools>
+        |Run query set:   Cli run <table-path> <format>
       """.stripMargin)
     sys.exit(42)
   }
 
+  def getSqlCtx(appName: String): SQLContext = {
+    val conf = new SparkConf().setAppName(appName)
+    val sc = new SparkContext(conf)
+    new HiveContext(sc)
+  }
+
   def main(args: Array[String]) {
     args match {
-      case Array("generate", master, tablePath, format, scaleFactor, tpcdsKitTools) =>
-        val sc = new SparkContext(master, "spark-sql-perf generate")
-        val sqlContext = new SQLContext(sc)
+      case Array("generate", tablePath, format, scaleFactor, tpcdsKitTools) =>
+        val sqlContext = getSqlCtx("spark-sql-perf generate")
         val tables = new Tables(sqlContext, tpcdsKitTools, scaleFactor.toInt)
-        tables.genData(tablePath, format, false, true, false, false, false)
-      case Array("run", master, tablePath, format) =>
-        val sc = new SparkContext(master, "spark-sql-perf run")
-        val sqlContext = new SQLContext(sc)
+        tables.genData(tablePath, format, false, false, false, false, false)
+      case Array("run", tablePath, format) =>
+        val sqlContext = getSqlCtx("spark-sql-perf run")
         val tables = new Tables(sqlContext, null, 0) //dsdgen dir and scale factor are not needed
         tables.createTemporaryTables(tablePath, format)
         val tpcds = new TPCDS(sqlContext) // defaults to results in /spark/sql/performance/*/
